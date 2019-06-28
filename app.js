a8586d30aa172a7ef4911c5f08b963d1952d700b
Jayme Desrosiers <jayme.desrosiers@gmail.com>
2019-06-28 15:29:49 -0500

    Expanded routing and added a 404 page.

--- app.js
+++ app.js
@@ -3,23 +3,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  console.log('Add Product Middleware running!');
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name-"title" /><button type="submit">Add Product</button></form>'
-  );
-});
+// Routes here
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('Root Middleware running!');
-  res.send('<h1>Root Page</h1>');
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Not Found</h1>');
 });
 
 app.listen(3000);
