c3de3198bfee604830692b02a5e27ddf868cf669
Jayme Desrosiers <jayme.desrosiers@gmail.com>
2019-06-28 16:33:41 -0500

    Added HTML for Add Product and Shop.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes here
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
