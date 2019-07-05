f882a3e4b311cd9ae58211fa3539ba12f8cc8404
Jayme Desrosiers <jayme.desrosiers@gmail.com>
2019-07-05 16:38:46 -0500

    Added a 404 page.

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
+const rootDir = require('../util/path');
 const express = require('express');
 const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
