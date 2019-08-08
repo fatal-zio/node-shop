1f30756c4ca0b5b83e8e90a07e2f761c995f9751
Jayme Desrosiers <jayme.desrosiers@gmail.com>
2019-08-08 16:04:39 -0500

    Updated styling and added dummy data support.

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,18 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
+  prodycts.push({
+    title: req.body.title
+  });
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,10 @@ const rootDir = require('../util/path');
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
+  console.log('shop.js', adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
